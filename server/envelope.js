fb8b99ab82ec395ddc8f683885a7dfea08a4810c
Jake Gabriel <73751365+atamalakrah@users.noreply.github.com>
2023-10-30 22:12:01 -0400

    update envelope.js
    +added put to update envelopes
    +added helper function for updating the right envelope

--- server/envelope.js
+++ server/envelope.js
@@ -4,8 +4,18 @@ module.exports = envelopesRouter;
 
 let envelopesDb = []
 
+function updateEnvelopeById(id, updatedEnvelope){
+    const index = envelopesDb.findIndex(envelope => envelope.id === id);
+    if(index !== -1){
+        envelopesDb[index] = {id: envelopesDb[index].id, budget: updatedEnvelope.budget, title: updatedEnvelope.title};
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 envelopesRouter.param('id', (req, res, next, id) => {
-    console.log('got here');
     const envelope = envelopesDb.find(envelope => envelope.id === parseInt(id));
     console.log(envelope);
     if(envelope){
@@ -32,4 +42,18 @@ envelopesRouter.get('/', (req, res, next) => {
 
 envelopesRouter.get('/:id', (req, res, next) => {
     res.send(req.envelope);
+});
+
+envelopesRouter.put('/:id', (req, res, next) => {
+    
+    let envelopeUpdate = {id: req.envelope.id, title: req.query.title, budget: req.query.budget};
+    console.log('Put test:');
+    console.log(envelopeUpdate);
+    if(updateEnvelopeById(req.envelope.id, envelopeUpdate)){
+        res.status(200).send(envelopeUpdate);
+    }
+    else{
+        res.status(404).send('Envelope update unsuccessful')
+    }
+    
 });
