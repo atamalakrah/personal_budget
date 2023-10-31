4e954f5dc53e724e2ce5040e1fe28ecff23a52bb
Jake Gabriel <73751365+atamalakrah@users.noreply.github.com>
2023-10-30 21:39:13 -0400

    update envelope.js
    +get to retrieve specific envelope by id
    +param to ensure id is valid

--- server/envelope.js
+++ server/envelope.js
@@ -4,15 +4,32 @@ module.exports = envelopesRouter;
 
 let envelopesDb = []
 
+envelopesRouter.param('id', (req, res, next, id) => {
+    console.log('got here');
+    const envelope = envelopesDb.find(envelope => envelope.id === parseInt(id));
+    console.log(envelope);
+    if(envelope){
+        req.envelope = envelope;
+        next();
+    }
+    else{
+        res.status(404).send();
+    }
+});
+
 envelopesRouter.post('/', (req, res, next) => {
-    const id = req.query.id;
     const budget = req.query.budget;
     const title = req.query.title;
-    const envelope = {id: id, budget: budget, title: title};
+    const envelope = {id: envelopesDb.length, budget: budget, title: title};
     envelopesDb.push(envelope);
     res.status(201).send(envelope);
-});
+    }
+);
 
 envelopesRouter.get('/', (req, res, next) => {
     res.send(envelopesDb);
+});
+
+envelopesRouter.get('/:id', (req, res, next) => {
+    res.send(req.envelope);
 });
